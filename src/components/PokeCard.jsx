833ab00e42b9105f73e4d144f520c0c631562432
Kyxk <kylian.gronnier@gmail.com>
2023-04-11 21:26:22 +0200

    cool la ternaire

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
-export default function PokeCard({ nom }) {
-  return (
+export default function PokeCard({ pokemon })
+{  
+return (
     <>
-      <figure className="card">
-        <img
-          src={`${nom}.png`}
-          alt={nom}
-          className="card-img"
-        />
-        <figcaption>{nom}</figcaption>
-      </figure>
-    </>
+      <figure className={`card ${pokemon.type === 'fire' ? 'bg-red-500' : pokemon.type === 'water' ? 'bg-blue-500' : pokemon.type === 'grass' ? 'bg-green-500' : 'bg-gray-500'}`}>
+  <img
+    src={`${pokemon.name}.png`}
+    alt={pokemon.name}
+    className="card-img"
+    onError={(e) => { e.target.onerror = null; e.target.src = 'unknown.png' }}
+  />
+  <figcaption>{pokemon.name}</figcaption>
+  <p>type:{pokemon.type ?? 'unknown'}</p>
+  <p>health:{pokemon.health ?? 'unknown'}</p>
+  <p>attack:{pokemon.attack ?? 'unknown'}</p>
+  <p>defense:{pokemon.defense ?? 'unknown'}</p>
+</figure>
+      
+      </>
+    
   );
 }
--- src/components/PokeCardList.jsx
+++ src/components/PokeCardList.jsx
@@ -1,12 +1,39 @@
 import PokeCard from "./PokeCard";
 
 export default function PokeCardList() {
-  const pokemons = ["charmander", "squirtle", "bulbasaur"];
+  const pokemons = [
+    {
+      name: "charmander",
+      type: "fire",
+      health: 50,
+      attack: 30,
+      defense: 20
+    },
+    {
+      name: "squirtle",
+      type: "water",
+      health: 60,
+      attack: 20,
+      defense: 30
+    },
+    {
+      name: "bulbasaur",
+      type: "grass",
+      health: 70,
+      attack: 25,
+      defense: 25
+      },
+      {
+      name: "new",
+      
+      
+    }
+  ];
 
   return (
     <>
       {pokemons.map((pokemon) => (
-        <PokeCard nom={pokemon} key={pokemon} />
+        <PokeCard pokemon={pokemon} key={pokemon} />
       ))}
     </>
   );
