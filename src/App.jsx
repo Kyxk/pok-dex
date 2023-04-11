2cf81c5c1d0a634033ed2b250116657675cd03c5
Kyxk <kylian.gronnier@gmail.com>
2023-04-11 20:29:10 +0200

    "yo flowbite"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './assets/css/App.css'
 import PokeCardList from "./components/PokeCardList";
-
-
+import { DatePicker } from 'antd';
+import Header from './components/Header';
+import Footer from './components/FooterApp';
 
 export default function App() {
   
   return (
     <>
       <div className="App">
+        <header>
+          <Header />
+        </header>
+        <main>
         <PokeCardList />
+        <DatePicker /> 
+        </main>
+        <footer>
+          <Footer />       
+        </footer>
       </div>
       
     </>
